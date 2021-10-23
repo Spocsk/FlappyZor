0166fb41a4816f4790cb76cfebb80df2c50a4d3e
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2021-10-23 16:36:40 +0200

    .

---
+++ Models/BirdModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FlappyZor.Models
+{
+    public class BirdModel
+    {
+
+        public int DsitanceFromGround { get; set;} = 100;
+
+        public BirdModel()
+        {
+        }
+    }
+}
+
---
+++ Models/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace FlappyZor.Models
+{
+    public class GameManager
+    {
+
+        public BirdModel Bird { get; set; }
+
+        public GameManager()
+        {
+            Bird = new BirdModel();
+        }
+    }
+}
+
